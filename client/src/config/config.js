dd538bf72fca53620688dc2fb04ab1da4534a602
soher <soher.vatik@gmail.com>
2023-04-20 14:34:54 +0300

    client1

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://ai-ej5i.onrender.com/api/v1/dalle",
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
   },
 };
 
